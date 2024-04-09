3bf2de1fc1727c36337652704a8c13d97b07a201
mod <mod>
2024-04-09 12:37:19 +0000

    shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,15 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import { Button } from "@/components/ui/button"
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
+      <div className='flex bg-cyan-300'>
+        <Button>Click me</Button>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
